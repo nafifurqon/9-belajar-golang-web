b9c1cef970e45af76fdf918a1e3d8a917290aca8
Muhammad Nafi' Furqon Diani <muhammadnafi98@gmail.com>
2025-07-02 09:54:29 +0700

    3. servemux - init

---
+++ 3-servemux/handler_test.go
@@ -0,0 +1,54 @@
+package servemux
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request) {
+		// logic web
+		fmt.Fprint(writer, "Hello World")
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8010",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Hello World")
+	})
+
+	mux.HandleFunc("/hi", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Hi")
+	})
+
+	mux.HandleFunc("/images/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Image")
+	})
+
+	mux.HandleFunc("/images/thumbnails/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Thumbnail")
+	})
+
+	server := http.Server{
+		Addr:    "localhost:8010",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
