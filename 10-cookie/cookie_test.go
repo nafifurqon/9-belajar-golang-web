6626fc2134d54c87f6ef3085ba9e1f71e5dfbf72
Muhammad Nafi' Furqon Diani <muhammadnafi98@gmail.com>
2025-07-02 19:43:15 +0700

    10. cookie - init

---
+++ 10-cookie/cookie_test.go
@@ -0,0 +1,77 @@
+package cookie
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func SetCookie(writer http.ResponseWriter, request *http.Request) {
+	cookie := new(http.Cookie)
+	cookie.Name = "X-Nafi-Name"
+	cookie.Value = request.URL.Query().Get("name")
+	cookie.Path = "/"
+
+	http.SetCookie(writer, cookie)
+	fmt.Fprint(writer, "Success create cookie")
+}
+
+func GetCookie(writer http.ResponseWriter, request *http.Request) {
+	cookie, err := request.Cookie("X-Nafi-Name")
+	if err != nil {
+		fmt.Fprint(writer, "No cookie")
+	} else {
+		name := cookie.Value
+		fmt.Fprintf(writer, "Hello %s", name)
+	}
+}
+
+func TestCookie(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/set-cookie", SetCookie)
+	mux.HandleFunc("/get-cookie", GetCookie)
+
+	server := http.Server{
+		Addr:    "localhost:8010",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestSetCookie(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8010/set-cookie?name=Nafi", nil)
+	recorder := httptest.NewRecorder()
+
+	SetCookie(recorder, request)
+
+	cookies := recorder.Result().Cookies()
+
+	for _, cookie := range cookies {
+		fmt.Printf("Cookise %s: %s \n", cookie.Name, cookie.Value)
+	}
+}
+
+func TestGetCookie(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8010/set-cookie?name=Nafi", nil)
+	cookie := new(http.Cookie)
+	cookie.Name = "X-Nafi-Name"
+	cookie.Value = "Nafi"
+	cookie.Path = "/"
+	request.AddCookie(cookie)
+
+	recorder := httptest.NewRecorder()
+
+	GetCookie(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	bodyString := string(body)
+
+	fmt.Println(bodyString)
+}
