61817327dbffa5fb67beadeb0f3b84e6534af69d
Muhammad Nafi' Furqon Diani <muhammadnafi98@gmail.com>
2025-07-03 09:25:10 +0700

    21. upload file - init

---
+++ 21-upload-file/upload_file_test.go
@@ -0,0 +1,85 @@
+package uploadfile
+
+import (
+	"bytes"
+	"embed"
+	_ "embed"
+	"fmt"
+	"html/template"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+//go:embed templates/*.gohtml
+var templates embed.FS
+
+var myTemplates = template.Must(template.ParseFS(templates, "templates/*.gohtml"))
+
+func UploadFile(writer http.ResponseWriter, request *http.Request) {
+	myTemplates.ExecuteTemplate(writer, "upload.form.gohtml", nil)
+}
+
+func Upload(writer http.ResponseWriter, request *http.Request) {
+	// request.ParseMultipartForm(32 << 20) // default 32 MB
+	// request.ParseMultipartForm(100 << 20) // ini diubah jadi 100 MB
+	file, fileHeader, err := request.FormFile("file")
+	if err != nil {
+		panic(err)
+	}
+	fileDestinastion, err := os.Create("./resources/" + fileHeader.Filename)
+	if err != nil {
+		panic(err)
+	}
+	_, err = io.Copy(fileDestinastion, file)
+	if err != nil {
+		panic(err)
+	}
+	name := request.PostFormValue("name")
+	myTemplates.ExecuteTemplate(writer, "upload.success.gohtml", map[string]interface{}{
+		"Name": name,
+		"File": "/static/" + fileHeader.Filename,
+	})
+}
+
+func TestUploadForm(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", UploadFile)
+	mux.HandleFunc("/upload", Upload)
+	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./resources"))))
+
+	server := http.Server{
+		Addr:    "localhost:8010",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+//go:embed resources/600x600.jpeg
+var uploadFileTest []byte
+
+func TestUploadFile(t *testing.T) {
+	body := new(bytes.Buffer)
+
+	writer := multipart.NewWriter(body)
+	writer.WriteField("name", "Muhammad Nafi Furqon")
+	file, _ := writer.CreateFormFile("file", "CONTOHUPLOAD.png")
+	file.Write(uploadFileTest)
+	writer.Close()
+
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8010/upload", body)
+	request.Header.Set("Content-Type", writer.FormDataContentType())
+	recorder := httptest.NewRecorder()
+
+	Upload(recorder, request)
+
+	bodyResponse, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(bodyResponse))
+}
